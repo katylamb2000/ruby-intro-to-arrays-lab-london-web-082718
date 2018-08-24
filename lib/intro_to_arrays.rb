6c4e0a7bf7d0830c5ef5470281d6ff2ddd696f4b
katy@karstaway.com <katy@karstaway.com>
2018-08-24 10:21:00 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array 
+  my_new_array = []
+end
+
+def array_with_two_elements
+  my_two_array =[1,2]
+end
+
+def first_element (indexing)
+ indexing[0]
+end 
+
+def third_element (indexing)
+  indexing[2]
+end
+
+def last_element (indexing)
+  indexing.last
+end
+
+def first_element_with_array_methods (south_east_asia)
+  south_east_asia.first
+end
+
+def last_element_with_array_methods (array)
+  array.last
+end
+
+def length_of_array (programming_languages)
+  programming_languages.length
+end
